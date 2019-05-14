a1710e7862a975141bf70488ae03cf06d3495c77
Inese Vilande <inesevilande1@gmail.com>
2019-05-14 18:01:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 var addElementToBeginningOfArray
 var array = ["Inese", "Sintija", "Martin"]
-chocolateBars.unshift("array")
+chocolateBars.unshift("Vija")
 
 

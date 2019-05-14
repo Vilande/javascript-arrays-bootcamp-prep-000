19be0ddb1fd8004677044c2dd076b5c460a8f51a
Inese Vilande <inesevilande1@gmail.com>
2019-05-14 19:54:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray (chocolateBars ,twix ){
 return [twix, ... chocolateBars]
 }
+
+function 

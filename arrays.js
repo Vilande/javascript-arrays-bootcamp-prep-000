b71685c4e2717f8170f2adf746bd9a1b9d658034
Inese Vilande <inesevilande1@gmail.com>
2019-05-14 18:12:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 //Function addElementToBeginningOfArray(chocolateBars){
 //return [double decker, ...chocolateBars]

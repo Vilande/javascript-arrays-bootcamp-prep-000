a8ae61e7de122eaf98b0ca31f64dfef6810bce26
Inese Vilande <inesevilande1@gmail.com>
2019-05-14 20:21:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 }
 
 function removeElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.slice(1)
+  chocolateBars.slice(0)
   return chocolateBars
 }
 

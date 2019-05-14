3f45edeba29a996810571143427ad6df198dd9e9
Inese Vilande <inesevilande1@gmail.com>
2019-05-14 18:09:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-Function addElementToBeginningOfArray(chocolateBars)
+Function addElementToBeginningOfArray(chocolateBars){
 return
 
 

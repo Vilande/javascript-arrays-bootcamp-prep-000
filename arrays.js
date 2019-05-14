93545ae6e0c012aec9d7ec17a1f413c0de0edfe9
Inese Vilande <inesevilande1@gmail.com>
2019-05-14 20:00:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ return [twix, ... chocolateBars]
 }
 
 
-
-//function destructivelyAddElementToBeginningOfArray(chocolateBars, mars){
-  //return chocolateBars.unshift("mars")
-//}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, mars){
+ chocolateBars.unshift("mars")
+ 
+}

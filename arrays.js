bb8b51e59f8006c635cd011be4917c2b49122a1b
Inese Vilande <inesevilande1@gmail.com>
2019-05-14 18:05:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-var addElementToBeginningOfArray
+Function (addElementToBeginningOfArray) 
 var array = ["Inese", "Sintija", "Martin"]
 chocolateBars.unshift("Vija")
 
